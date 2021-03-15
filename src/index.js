1affa5aaae8f0fddf0ab591f44843b68607efbfc
Aleksey-Anatolich <59198517+Aleksey-Anatolich@users.noreply.github.com>
2021-03-16 00:43:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let rev = 0;
+        n = (n < 0) ? n * -1 : n;
+       while (n) {
+           rev = rev * 10 + n % 10;
+           n = Math.floor(n / 10);
+       }
+
+       return rev;
 }
